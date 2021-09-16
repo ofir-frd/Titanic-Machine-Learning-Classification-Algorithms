e932604469c54fac9e12cb8a2a16d2d1f6505457
ofir-frd <85901822+ofir-frd@users.noreply.github.com>
2021-09-16 15:40:28 +0300

    Create main.py

---
+++ script/main.py
@@ -0,0 +1,39 @@
+
+
+"""""""""""
+Titatic Project
+
+
+DF description:
+    
+Variable	Definition	          Key
+survival 	Survival 	            0 = No, 1 = Yes
+pclass 	    Ticket class 	      1 = 1st, 2 = 2nd, 3 = 3rd
+sex 	    Sex 	
+Age 	    Age in years 	
+sibsp 	    # of siblings / spouses aboard the Titanic 	
+parch 	    # of parents / children aboard the Titanic 	
+ticket 	    Ticket number 	
+fare 	    Passenger fare 	
+cabin 	    Cabin number 	
+embarked 	Port of Embarkation 	C = Cherbourg, Q = Queenstown, S = Southampton
+
+
+
+"""""""""""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sn
+
+
+
+
+def main():
+    
+
+    
+    
+if __name__ == "__main__":
+    main()
