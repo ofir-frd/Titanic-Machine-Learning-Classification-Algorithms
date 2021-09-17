64083cdfa08a76e9aea7636f0a2753e0bdc95603
ofir-frd <85901822+ofir-frd@users.noreply.github.com>
2021-09-17 15:04:17 +0300

    Update main.py

--- script/main.py
+++ script/main.py
@@ -6,17 +6,17 @@ Titatic Project
 
 DF description:
     
-Variable	Definition	          Key
+Variable	Definition	            Key
 survival 	Survival 	            0 = No, 1 = Yes
-pclass 	    Ticket class 	      1 = 1st, 2 = 2nd, 3 = 3rd
-sex 	    Sex 	
+pclass 	    Ticket class 	        1 = 1st, 2 = 2nd, 3 = 3rd
+sex 	    Sex 	                0 = female, 1= male
 Age 	    Age in years 	
 sibsp 	    # of siblings / spouses aboard the Titanic 	
 parch 	    # of parents / children aboard the Titanic 	
 ticket 	    Ticket number 	
 fare 	    Passenger fare 	
 cabin 	    Cabin number 	
-embarked 	Port of Embarkation 	C = Cherbourg, Q = Queenstown, S = Southampton
+embarked 	Port of Embarkation 	C/0 = Cherbourg, Q/1 = Queenstown, S/2 = Southampton
 
 
 
@@ -28,6 +28,27 @@ import matplotlib.pyplot as plt
 import seaborn as sn
 
 
+
+### Clean and organize raw data
+def titaticDFPreProcessing(titaticDF):
+
+    
+    ### Remove feature that are irrelevant for survivability.
+    ### Consider Port of Embarkation removal due to insignificance.
+    
+    titaticDF = titaticDF.drop('Name', axis=1)
+    titaticDF = titaticDF.drop('Ticket', axis=1)
+    titaticDF = titaticDF.drop('Cabin', axis=1)      # consider use in the future (78% missing data)
+    
+    ### Transform all features to numbers
+    
+    titaticDF["Sex"] = titaticDF["Sex"].apply(lambda x: 0 if x == 'female' else 1) # female/male to 0/1
+    titaticDF["Embarked"] = titaticDF["Embarked"].apply(lambda x: 0 if x == 'C' else ( 1 if x == 'Q' else 2)) # C to 0, Q to 1, S to 2
+
+
+
+
+
 def main():
     
     ### Initial statues of the data
@@ -36,12 +57,12 @@ def main():
     
     titaticDFFeatureList= []
     
-    [titaticDFFeatureList.append(feature) for feature in titaticDF.columns if titaticDF[feature].isnull().sum()>1]
+    [titaticDFFeatureList.append(feature) for feature in titaticDF.columns]
           
     for feature in titaticDFFeatureList:
         print("{} had {} % missing values".format(feature,np.round(titaticDF[feature].isnull().sum()/len(titaticDF)*100,2)))
     
-
+     titaticDF = titaticDFPreProcessing(titaticDF)  # Initiate DF pre-processing
     
     
 if __name__ == "__main__":
