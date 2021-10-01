3ae55574d958d1713b729639edaffd52a8104a2b
ofir-frd <85901822+ofir-frd@users.noreply.github.com>
2021-10-01 11:31:05 +0300

    Update main.py

--- script/main.py
+++ script/main.py
@@ -11,8 +11,8 @@ survival 	Survival 	            0 = No, 1 = Yes
 pclass 	    Ticket class 	        1 = 1st, 2 = 2nd, 3 = 3rd
 sex 	    Sex 	                0 = female, 1= male
 Age 	    Age in years 	
-sibsp 	    # of siblings / spouses aboard the Titanic 	
-parch 	    # of parents / children aboard the Titanic 	
+Tibsp 	    # of siblings / spouses aboard the Titanic 	
+Parch 	    # of parents / children aboard the Titanic 	
 ticket 	    Ticket number 	
 fare 	    Passenger fare 	
 cabin 	    Cabin number 	
@@ -105,7 +105,24 @@ def plotProcessedData(titaticTestDF, titaticTrainDF):
     
     plt.show()
        
+    
+    ### The distribution by number of parents/children of dead and alive passangers
+    
+    fig05, axis = plt.subplots(1, 2)
 
+    axis[0].hist(titaticTrainDF.loc[titaticTrainDF['Survived'] == 0, :]['Parch'], color = 'royalblue')
+    axis[1].hist(titaticTrainDF.loc[titaticTrainDF['Survived'] == 1, :]['Parch'], color = 'darkorange')
+    
+    axis[0].set_title('Died passengers')
+    axis[0].set_xlabel("Num. of parents/children")
+    
+    axis[1].set_title('Survived passengers')
+    axis[1].set_xlabel("Num. of parents/children")
+    
+    plt.show()
+    
+
+    
 ### Clean and organize raw data
 def titaticDFPreProcessing(titaticDF):
 
